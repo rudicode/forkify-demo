76520411520f0f7bb9f35fd771b5e7d5773ce110
Rudi Luzar <rudi@rudicode.com>
2022-11-27 07:43:05 -0500

    Completed loading recipe, and displaying it on page.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,156 @@
+// use parcel v2 to import paths to resources
+import imgIicons from 'url:../img/icons.svg' // parcel v2
+console.log(imgIicons); // this is the path to the icons in /dist directory
+
+///////////////////////////////////////
+
+import * as simpleCache from './simpleCache.js';
+
+///////////////////////////////////////
+
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+    return new Promise(function (_, reject) {
+        setTimeout(function () {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
 };
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+///////////////////////////////////////
+
+const showRecipe = async function (useCached = false) {
+    // loading recipe
+    try {
+        let data;
+        if (useCached) {
+            data = simpleCache.recipe2;
+            console.log(`Using cached recipe.`);
+        } else {
+            const res = await fetch(
+                // `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+                `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc958`
+            );
+            data = await res.json();
+            console.log(res, data);
+            if (!res.ok) throw new Error(`${data.message} (${res.status})`)
+        }
+
+        let { recipe } = data.data
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients,
+        }
+        console.log(recipe);
+        // rendernig recipe
+        const html = `
+            <figure class="recipe__fig">
+          <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
+          <h1 class="recipe__title">
+            <span>${recipe.title}</span>
+          </h1>
+        </figure>
+
+        <div class="recipe__details">
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="${imgIicons}#icon-clock"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+            <span class="recipe__info-text">minutes</span>
+          </div>
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="${imgIicons}#icon-users"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+            <span class="recipe__info-text">servings</span>
+
+            <div class="recipe__info-buttons">
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="${imgIicons}#icon-minus-circle"></use>
+                </svg>
+              </button>
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="${imgIicons}#icon-plus-circle"></use>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div class="recipe__user-generated">
+            <svg>
+              <use href="${imgIicons}#icon-user"></use>
+            </svg>
+          </div>
+          <button class="btn--round">
+            <svg class="">
+              <use href="${imgIicons}#icon-bookmark-fill"></use>
+            </svg>
+          </button>
+        </div>
+
+        <div class="recipe__ingredients">
+          <h2 class="heading--2">Recipe ingredients</h2>
+          <ul class="recipe__ingredient-list">
+            ${recipe.ingredients.map(ing => {
+            return `
+                <li class="recipe__ingredient">
+                  <svg class="recipe__icon">
+                    <use href="${imgIicons}#icon-check"></use>
+                  </svg>
+                  <div class="recipe__quantity">${ing.quantity}</div>
+                  <div class="recipe__description">
+                    <span class="recipe__unit">${ing.unit}</span>
+                    ${ing.description}
+                  </div>
+                </li>
+                `
+        }).join('\n')
+            }
+          </ul>
+        </div>
+
+        <div class="recipe__directions">
+          <h2 class="heading--2">How to cook it</h2>
+          <p class="recipe__directions-text">
+            This recipe was carefully designed and tested by
+            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+            directions at their website.
+          </p>
+          <a
+            class="btn--small recipe__btn"
+            href="${recipe.sourceUrl}"
+            target="_blank"
+          >
+            <span>Directions</span>
+            <svg class="search__icon">
+              <use href="${imgIicons}#icon-arrow-right"></use>
+            </svg>
+          </a>
+        </div>`;
+        recipeContainer.innerHTML = '';
+        recipeContainer.insertAdjacentHTML('afterbegin', html);
+
+    } catch (err) {
+        console.error(`Error: $showRecipe(), ${err.message}`);
+    }
+
+}
+
+console.log('Game ON!!');
+showRecipe(true);
+// console.log(simpleCache.searchPizza);
